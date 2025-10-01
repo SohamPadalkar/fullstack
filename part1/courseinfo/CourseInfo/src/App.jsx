05363a7447a7bc540b4172be35e3ccc4f6acd580
Soham Vilas Padalkar <bub56813@gmail.com>
2025-10-01 22:31:02 +0530

    1.2Exercise

--- part1/courseinfo/CourseInfo/src/App.jsx
+++ part1/courseinfo/CourseInfo/src/App.jsx
@@ -2,26 +2,31 @@ function Header(props) {
   return <h1>{props.course}</h1>
 }
 
+function Part(props) {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
 function Content(props) {
   return (
     <>
-    <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part name={props.part1} exercises={props.exercises1} />
+      <Part name={props.part2} exercises={props.exercises2} />
+      <Part name={props.part3} exercises={props.exercises3} />
     </>
   )
 }
 
 function Total(props) {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  return (
+    <p>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  )
 }
-
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -32,14 +37,22 @@ const App = () => {
   const part3 = 'State of component'
   const exercises3 = 14
 
-
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   )
 }
 
 export default App
+
